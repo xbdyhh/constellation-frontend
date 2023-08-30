ddd106a160659ea515f93974bce6b1d732163c08
panxinyang <1670016749@qq.com>
2023-08-31 06:50:28 +0800

    use_cors_lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,19 @@ import { Chain, AssetList } from '@chain-registry/types';
 import { defaultTheme } from '../config';
 import {nibiruChian,nibiruAssets} from '../config/nibiru_itn_1';
 import '@interchain-ui/react/styles';
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 function CreateCosmosApp({ Component, pageProps }: AppProps) {
   const signerOptions: SignerOptions = {
