30dd1b35774314f8b4133e45f4406bbba2f882ae
panxinyang <1670016749@qq.com>
2023-08-31 07:32:36 +0800

    fix_bug

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/.*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   reactStrictMode: false,
   swcMinify: true,
 }
-
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://rpc.itn-1.nibiru.fi/:path*'
-      }
-    ]
-  }
-}
-
-module.exports = nextConfig
