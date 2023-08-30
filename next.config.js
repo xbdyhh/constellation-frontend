e6af3e5fa697ad44806804616a2314273363a092
panxinyang <1670016749@qq.com>
2023-08-31 06:20:36 +0800

    fix_cors

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "https://rpc.itn-1.nibiru.fi/",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
