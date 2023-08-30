44ab32c677792ec40638ecd36205c1fe8b4adf5d
panxinyang <1670016749@qq.com>
2023-08-31 07:39:14 +0800

    fix_bug

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/.*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
